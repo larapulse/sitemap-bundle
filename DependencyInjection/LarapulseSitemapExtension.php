5b646355668a106443b5500a47bd92ed5f5df86d
Sergey Podgornyy <Serhii.Podhornyi@check24.de>
2017-11-23 19:47:05 +0100

    Rename extension file

--- DependencyInjection/LarapulseSitemapExtension.php
+++ DependencyInjection/LarapulseSitemapExtension.php
@@ -7,7 +7,7 @@ use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\Config\FileLocator;
 
-class SitemapExtension extends Extension
+class LarapulseSitemapExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container)
     {
